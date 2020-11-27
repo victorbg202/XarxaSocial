a1fcf3a03902072582e00e251e727ffaa9512ac2
victorbg202 <victorbg202@gmail.com>
2020-11-27 19:31:03 +0100

    27/11/2020

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistreComponent } from './components/registre/registre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", pathMatch: "full", redirectTo: "inici"},
+  { path: 'registro' , component: RegistreComponent },
+  {path: "**" ,redirectTo: "inicio"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/registre/registre.component.ts
+++ src/app/components/registre/registre.component.ts
@@ -7,9 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistreComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  errLloc: string = "";
+  errNom: string = "";
+  errEmail = ['^[^@]+@[^@]+\.[a-zA-Z]{2,}$'];
+  errText: string = "";
+  mostrarInfo: string = "";
+  errCheck?;
+
+  enviarDatos(nom, cognom, url, text, email, descripcio) {
+
+    //Comp nombre
+    if(nom == ""){
+      this.errNom = "Nom incorrecte introdueix un nom";
+    }else {
+      this.errNom = "Nom correct";
+    }
+
+    //Comp Apellidos
+    if(cognom == ""){
+      this.errLloc = "Cognom incorrecte introdueix un lloc";
+    }else {
+      this.errLloc = "Lloc correct";
+    }
+
+    //Comp email
+    if(email.match(this.errEmail)){
+      this.mostrarInfo = "Email correcto";
+    }else{
+      this.mostrarInfo = "Email incorrecto";
+    }
+
+    //
+    if(descripcio == ""){
+      this.errLloc = "Descripcio incorrecte introdueix una descripcio";
+    }else {
+      if(text.length < 2){
+        this.errText = "El text es molt curt";
+      }else {
+        this.errText = "Text correcte";
+      }
+    }
+
+    if (this.errCheck = document.getElementById("Check")/*.checked*/ && nom != "" &&  lloc!="" && email.match(this.errEmail)  && text.length > 2) {
+      this.errCheck = "";
+      //this.carta.push(new GrupEvents(nom, lloc, url , text, email));
+    }else {
+      this.errCheck = "Acepar condicions";
+    }
   }
 
 }
