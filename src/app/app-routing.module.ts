ea5d857cb540c5a19a713ed07b4edb1d23b1b238
Marc Batalla Tapia <57674196+MarcB98@users.noreply.github.com>
2020-11-27 19:30:24 +0100

    1r commit
    Apartat de Administracio
    
    by: MarcB98

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdministradorComponent } from './components/administrador/administrador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", pathMatch: "full", redirectTo:"inici"},
+  {path: 'admin', component: AdministradorComponent},
+  {path: "**" , redirectTo:"inici"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
