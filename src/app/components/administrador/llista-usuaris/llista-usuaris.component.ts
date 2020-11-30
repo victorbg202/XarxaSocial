83e5959a84f9bd43b93224aa87e87592324d87b8
MarcB98 <marcbatalla652@gmail.com>
2020-11-30 19:45:20 +0100

    Lista Usuarios
    Apartat de Administrador feta!

--- src/app/components/administrador/llista-usuaris/llista-usuaris.component.ts
+++ src/app/components/administrador/llista-usuaris/llista-usuaris.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserListAdmin } from 'src/app/models/lista-admin-users.model';
 
 @Component({
   selector: 'app-llista-usuaris',
@@ -7,9 +8,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LlistaUsuarisComponent implements OnInit {
 
+  users_admin: UserListAdmin = null;
+  listaUsers: UserListAdmin[] = [];
+  listaUsers1: UserListAdmin[] = [];
+  private desc:string ="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
+  private rut_img:string ="../../../../assets/img/icono_user.png";
+
   constructor() { }
 
   ngOnInit(): void {
+    this.listaUsers.push(new UserListAdmin(
+      Math.round(Math.random()*100),
+      'Marc',
+      'Batalla',
+      22,
+      this.rut_img,
+      this.desc,
+      'marcbatalla@gmail.com')
+    );
+    this.listaUsers.push(new UserListAdmin(
+      Math.round(Math.random()*100),
+      'Victor',
+      'Barbe',
+      21,
+      this.rut_img,
+      this.desc,
+      'victorbarbe@gmail.com')
+      );
+    this.listaUsers.push(new UserListAdmin(Math.round(
+      Math.random()*100),
+      'Quim',
+      'Martinez',
+      20,
+      this.rut_img,
+      this.desc,
+      'quimmartinez@gmail.com')
+      );
+    this.listaUsers.push(new UserListAdmin(Math.round(
+      Math.random()*100),
+      'Valenti',
+      'Casas',
+      30,
+      this.rut_img,
+      this.desc,
+      'vcasas@ilerna.com')
+      );
+    this.listaUsers.push(new UserListAdmin(
+      Math.round(Math.random()*100),
+      'Nelson',
+      'Perez',
+      30,
+      this.rut_img,
+      this.desc,
+      'nperez@ilerna.com')
+      );
+  }
+
+  borrar(id){
+    console.log(id);
+    for (let index = 0; index < this.listaUsers.length; index++) {
+      if (this.listaUsers[index].id_user == id) {
+        this.listaUsers.splice(index, 1);
+      }
+    }
   }
 
 }
---
+++ src/app/models/lista-admin-users.model.ts
@@ -0,0 +1,14 @@
+export class UserListAdmin
+{
+
+  constructor(
+    public id_user: number,
+    public nom_user: string,
+    public apellido_user: string,
+    public edat: number,
+    public foto: string,
+    public descripcio: string,
+    public correu: string
+  ){}
+
+}
