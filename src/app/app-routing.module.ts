ae36dae962d225c890d04200528b732cf2a604b9
quimmp26 <quiim2000@gmail.com>
2020-12-01 17:57:52 +0100

    model user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetallsPerfilComponent } from './components/perfil/detalls-perfil/detalls-perfil.component';
+import { LlistaPerfilsComponent } from './components/perfil/llista-perfils/llista-perfils.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'detalls-perfil', component: DetallsPerfilComponent },
+  { path: 'llista-perfils', component: LlistaPerfilsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/perfil/llista-perfils/llista-perfils.component.ts
+++ src/app/components/perfil/llista-perfils/llista-perfils.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-llista-perfils',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LlistaPerfilsComponent implements OnInit {
 
-  constructor() { }
+  usuarios: User[] = [];
+
+  constructor() {
+
+    this.usuarios.push(new User('Quim','Martínez',20,'assets/img/1.jpg','Programador Fullstack', 'quiim2000@gmail.com', 'qmartinez'));
+    this.usuarios.push(new User('Victor','Barbé',21,'assets/img/2.jpg','Programador Frontend', 'victorbg202@gmail.com', 'vbarbe'));
+    this.usuarios.push(new User('Marc','Batalla',22,'assets/img/3.jpg','Programador Backend', 'marcbatalla@gmail.com', 'mbatalla'));
+  }
 
   ngOnInit(): void {
+
+
+
   }
 
 }
