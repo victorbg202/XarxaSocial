1f177401792002c0150306b235501d8f1c6682ef
victorbg202 <victorbg202@gmail.com>
2020-12-07 11:19:19 +0100

    Registre finalitzat
    Pendent merge amb altres parts

--- src/app/components/registre/registre.component.ts
+++ src/app/components/registre/registre.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { RxReactiveFormsModule, RxwebValidators } from '@rxweb/reactive-form-validators';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -7,99 +9,29 @@ import Swal from 'sweetalert2';
   styleUrls: ['./registre.component.css']
 })
 
-export class RegistreComponent implements OnInit {
+export class RegistreComponent {
 
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+    registre = new FormGroup({
+      nom: new FormControl('', Validators.required),
+      cognom: new FormControl('', Validators.required),
+      edat: new FormControl('', Validators.required),
+      foto: new FormControl('', Validators.required),
+      desc: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      cont: new FormControl('', Validators.required),
+      confCont: new FormControl('', [Validators.required, RxwebValidators.compare({fieldName:'cont'})])
+    });
 
-  //
-  errLloc: string = "";
-  errNom: string = "";
-  errApellido = "";
-  errEdat = "";
-  errFoto = "";
-  errDesc = "";
-  errCont = "";
-  errEmail = "";
-  compEmail = ['^[^@]+@[^@]+\.[a-zA-Z]{2,}$'];
-  errText: string = "";
-  mostrarInfo: string = "";
-  errCheck?;
-
-  //Recibir y comprobar datos
-  enviarDatos(nom, cognom, edat, foto, desc, email, cont, confCont) {
-
-    //Comp nombre
-    if(nom == ""){
-      this.errNom = "Nom incorrecte introdueix un nom";
-    }else {
-      this.errNom = "Nom correcte";
-    }
-
-    //Comp Apellidos
-    if(cognom == ""){
-      this.errApellido = "Cognom incorrecte introdueix un lloc";
-    }else {
-      this.errApellido = "Cognom correct";
-    }
-
-    //Comp Edat
-    if(edat == ""){
-      this.errEdat = "Edat incorrecte introdueix una edat";
-    }else {
-      this.errEdat = "Edat correcta";
-    }
-
-    //Comp Foto
-    if(foto == ""){
-      this.errFoto = "Foto incorrecte introdueix una foto";
-    }else {
-      this.errFoto = "Foto correcta";
-    }
-
-    //Comp desc
-    if(desc == ""){
-      this.errDesc = "Descripcio incorrecte introdueix una descripcio";
-    }else {
-      if(desc.length < 4){
-        this.errText = "El text es molt curt";
-      }else {
-        this.errText = "Text correcte";
-      }
-    }
-
-    //Comp email
-    if(email != "" && email.match(this.compEmail)){
-      this.errEmail = "Email correcte";
-    }else{
-      this.errEmail = "Email incorrecte";
-    }
-
-    //Comp Cont
-    if(cont != "" && (cont=confCont)) {
-      this.errCont = "Contrasenya correcta";
-    }else {
-      this.errCont = "Contrasenya incorrecta";
-    }
-
-    //Push y feedback
-    if (email.match(this.compEmail) && (cont=confCont) && nom != "" &&  cognom!="" && edat!="" && foto!="" && email!="" && desc.length > 4 && cont!="") {
-      //Guardar usuario
-      //this.??.push(new ??(nom, cognom, edat, foto, desc, email, cont));
-
-      //Mensaje OK
+    sweetAlert() {
       Swal.fire({
         icon: 'success',
-        text: 'Registrat!'
-      })
-    }else {
-      //Mensaje de error
-      Swal.fire({
-        icon: 'error',
-        text: 'Alguna cosa ha anat malament!'
+        text: 'Usuari registrat!',
       })
     }
-  }
 
+    push() {
+      //push array
+    }
 }
+
+
